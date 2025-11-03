e3e6c1bbe30d1afbe1aa1ac8a7b43b90e3b0597f
SKirankumar45 <kirankumar02427@gmail.com>
2025-11-03 19:25:09 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,14 @@ export default function App() {
 
   const toggle = () => {
     if(isLight){
-      document.body.style.backgroundColor = "rgba(106, 106, 106, 1)";
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      document.getElementById("colorbtn").textContent = "light";
       setLight(false);
     }else{
       document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      document.getElementById("colorbtn").textContent = "dark";
       setLight(true);
     }
   }
@@ -45,9 +49,9 @@ export default function App() {
   return (
     <div className='App'>
       <header>
-      <h1 className='title'>Task Harmony &#128198;&#x2705;</h1>
-      <p className='tagline'>Your Friendly Task Manager</p>
-      <span><button onClick={toggle} className={"change-theme"}>change theme</button></span>
+      <h1 className='title color'>Task Harmony &#128198;&#x2705;</h1>
+      <p className='tagline color'>Your Friendly Task Manager</p>
+      <span><button onClick={toggle} className={"change-theme"} id="colorbtn">dark</button></span>
       </header>
       <Taskform addTask={addTask}/>
       <Tasklist tasks={tasks}
